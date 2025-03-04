4aaa50883d2cbaa70969d382fb0296886fd64ffe
전화철 <117714970+J-Cheol@users.noreply.github.com>
2025-03-04 13:48:57 +0900

    [Feature] 소셜로그인(Kakao) 기능 개발 (#11)
    * feat : Allow Kakao access path permissions
    
    * chore: add Kakao OAuth configuration to application-dev.yml
    
    * refactor: remove UserDetails from Member entity and implement separately
    
    * refactor : ApiResponse fix
    
    - static 메서드 기반이므로 객체 생성 없이 유틸성 클래스로 활용 가능
    - 응답 구조(ApiResult)를 정의하여 일관된 형식 유지 가능
    - API 응답 시 success, message 필드를 포함할 수 있어 확장성이 높음
    - accessTokenResponse()에서 HttpHeaders를 활용하여 명확한 헤더 추가
    
    * feat : Add Exceptions
    
    * feat : Add RestTemplateConfig
    
    * feat : Add Token Entity
    
    * feat: implement JWT token generation and authentication
    
    * chore: add @Component annotation to JwtAuthenticationFilter
    
    * feat : Add getCurrentMemberId
    
    * feat : update member functionality
    
    * feat : Add Kakao Login functionality
    
    * feat : Add reissue Token
    
    * refactor : kakao refactor
    
    * refactor: Rename variables
    
    * chore : application prod, default modify
    
    * chore : Add prod kakao
    
    * fix : SecurityConfig 누락 수정
    
    * chore : EnvironmentVariable modify

---
+++ src/main/java/com/traveljournal/domain/member/controller/TokenController.java
@@ -0,0 +1,40 @@
+package com.traveljournal.domain.member.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.traveljournal.domain.member.dto.MemberTokenResponse;
+import com.traveljournal.domain.member.dto.ReissueRequest;
+import com.traveljournal.domain.member.dto.ReissueResonse;
+import com.traveljournal.domain.member.service.TokenService;
+import com.traveljournal.global.data.ApiResponse;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/v1/tokens")
+@Tag(name = "token API", description = "토큰 관련 API")
+public class TokenController {
+
+	private final TokenService tokenService;
+
+	/**
+	 * 토큰 재발급 요청 처리
+	 * Refresh 토큰과 장치 ID를 사용하여 새로운 액세스 토큰 발급
+	 */
+	@Operation(
+		summary = "Token Refresh",
+		description = "Refresh 토큰과 장치 ID를 사용하여 새로운 액세스 토큰을 발급합니다."
+	)
+	@PostMapping("/reissue")
+	public ResponseEntity<ReissueResonse> refreshToken(@RequestBody ReissueRequest Reissuerequest) {
+		MemberTokenResponse memberTokenResponse = tokenService.refreshToken(Reissuerequest);
+		return ApiResponse.accessTokenResponse(ReissueResonse.of(memberTokenResponse), memberTokenResponse.tokenInfo().accessToken());
+	}
+}
