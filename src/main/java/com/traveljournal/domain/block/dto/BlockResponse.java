9d941189ea6f12ec7b87370d280dbd2f75bdb99d
전화철 <lunaris0206@naver.com>
2025-05-21 21:50:28 +0900

    feat : Implement BlockController Swagger docs

--- src/main/java/com/traveljournal/domain/block/dto/BlockResponse.java
+++ src/main/java/com/traveljournal/domain/block/dto/BlockResponse.java
@@ -1,19 +1,24 @@
 package com.traveljournal.domain.block.dto;
 
 import com.traveljournal.domain.member.entity.Member;
+
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 @Builder
 public record BlockResponse(
-        Long memberId,
-        String nickname,
-        String profileImageUrl
+	@Schema(example = "1")
+	Long memberId,
+	@Schema(example = "도요새")
+	String nickname,
+	@Schema(example = "https://travel-journal-s3.s3.amazonaws.com/default/profile/default1.png")
+	String profileImageUrl
 ) {
-    public static BlockResponse of(Member blocked) {
-        return BlockResponse.builder()
-                .memberId(blocked.getId())
-                .nickname(blocked.getNickname())
-                .profileImageUrl(blocked.getProfileImageUrl())
-                .build();
-    }
+	public static BlockResponse of(Member blocked) {
+		return BlockResponse.builder()
+			.memberId(blocked.getId())
+			.nickname(blocked.getNickname())
+			.profileImageUrl(blocked.getProfileImageUrl())
+			.build();
+	}
 }
