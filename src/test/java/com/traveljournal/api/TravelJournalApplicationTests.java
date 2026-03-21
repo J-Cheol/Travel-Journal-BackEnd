15de1d424f212f2d978e254bb993360d72363b06
전화철 <lunaris0206@naver.com>
2026-03-21 15:14:41 +0900

    chore: add test profile for CI reprodcuibility

--- src/test/java/com/traveljournal/api/TravelJournalApplicationTests.java
+++ src/test/java/com/traveljournal/api/TravelJournalApplicationTests.java
@@ -2,8 +2,10 @@ package com.traveljournal.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TravelJournalApplicationTests {
 
 	@Test
